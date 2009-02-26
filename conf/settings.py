8b4710e19502b3ba6b905ad96730106de71340af
Taylan Pince <taylanpince@gmail.com>
2009-02-26 02:00:15 -0500

    Added conf files and fabric deployment script.

---
+++ conf/settings.py
@@ -0,0 +1,8 @@
+DATABASE_ENGINE = 'mysql'
+DATABASE_NAME = 'orangeblog'
+DATABASE_USER = 'orangeblogdbu'
+DATABASE_PASSWORD = 'ZAYtSFGAKd'
+DATABASE_HOST = ''
+DATABASE_PORT = ''
+
+TIME_ZONE = 'Turkey'
